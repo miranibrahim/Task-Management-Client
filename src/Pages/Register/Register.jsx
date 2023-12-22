c3f8e90afbbe1af9369365bae6e48311bee1e4ae
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-12-22 12:36:10 +0600

    authentication pages completed

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,214 @@
-
+import { Link, useNavigate } from "react-router-dom";
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Register = () => {
-    return (
-        <div>
-            
+  const axiosPublic = useAxiosPublic();
+  const { createUser, updateUserProfile, googleSignIn } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+      updateUserProfile(data.name, data.photoURL)
+        .then(() => {
+          const newUser = {
+            email: data.email,
+            name: data.name,
+            role: "unsubscribed",
+          };
+          axiosPublic.post("/users", newUser).then((res) => {
+            if (res.data.insertedId) {
+              Swal.fire({
+                title: "Registration Completed",
+                showClass: {
+                  popup: `
+                  animate__animated
+                  animate__fadeInUp
+                  animate__faster
+                `,
+                },
+                hideClass: {
+                  popup: `
+                  animate__animated
+                  animate__fadeOutDown
+                  animate__faster
+                `,
+                },
+              });
+              reset();
+              navigate("/");
+            }
+          });
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+  const handleGoogleSignIn = () => {
+    googleSignIn().then((result) => {
+      const user = result.user;
+      const newUser = {
+        email: user.email,
+        name: user.displayName,
+        role: "unsubscribed",
+      };
+      axiosPublic.post("/users", newUser).then((res) => {
+        console.log(res.data);
+        navigate("/");
+      });
+    });
+  };
+  return (
+    <div className="flex justify-center items-center bg-gradient-to-r from-blue-400 via-black-200 via-blue-300 to-blue-500">
+      <div className="w-full max-w-md bg-black bg-opacity-30 bg-blend-screen py-8 px-10 mt-20 rounded-lg my-10">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-6">
+            <h1 className="text-4xl font-bold text-center">REGISTER</h1>
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              name="photoURL"
+              placeholder="Paste your photoURL *"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+              {...register("photoURL", {
+                required: "photoURL is required",
+              })}
+              aria-invalid={errors.name ? "true" : "false"}
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              name="name"
+              placeholder="User Name *"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+              {...register("name", {
+                required: "Name is required",
+              })}
+              aria-invalid={errors.name ? "true" : "false"}
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="mb-6">
+            <select
+              name="profession"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+              {...register("profession", {
+                required: "Profession is required",
+              })}
+              aria-invalid={errors.profession ? "true" : "false"}
+            >
+              <option value="" disabled>
+                Select Your Profession *
+              </option>
+              <option value="developer">Developer</option>
+              <option value="corporateProfessional">
+                Corporate Professional
+              </option>
+              <option value="banker">Banker</option>
+              <option value="student">Student</option>
+            </select>
+            {errors.profession && (
+              <p className="text-red-500">{errors.profession.message}</p>
+            )}
+          </div>
+
+          <div className="mb-6">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email ID *"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+              {...register("email", {
+                required: "Email Address is required",
+              })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div className="mb-6">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password *"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+                maxLength: 20,
+                pattern:
+                  /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/,
+              })}
+            />
+            {errors.password?.type === "required" && (
+              <p className="text-red-500">Password is required</p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p className="text-red-500">Minimum 6 character required</p>
+            )}
+            {errors.password?.type === "maxLength" && (
+              <p className="text-red-500">Maximum 20 character required</p>
+            )}
+            {errors.password?.type === "pattern" && (
+              <p className="text-red-800">
+                {" "}
+                *At least one uppercase letter, one lowercase letter, one number
+                and one special character required.*
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="bg-red-500 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Sign Up
+          </button>
+        </form>
+        <h2 className="text-lg text-white mt-3 font-semibold">
+          Already have an account? Please{" "}
+          <Link className="text-lime-300 underline" to="/login">
+            Log in
+          </Link>{" "}
+        </h2>
+        <div className="divider divider-warning text-white font-bold">
+          **OR**
         </div>
-    );
+        <div className="text-3xl flex justify-evenly bg-white p-3">
+          <button>
+            <FaFacebook></FaFacebook>
+          </button>
+          <button onClick={handleGoogleSignIn}>
+            <FaGoogle></FaGoogle>
+          </button>
+          <button>
+            <FaGithub></FaGithub>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,14 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import DarkModeToggle from "../Component/DarkMode/DarkModeToggle";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Navbar = () => {
-    const navItems = <>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/login'>Login</NavLink></li>
-        <li><NavLink to='/register'>Register</NavLink></li>
-        <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+  const navItems = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/register">Register</NavLink>
+      </li>
+      <li>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      </li>
     </>
+  );
   return (
-    <div className="navbar bg-base-100">
+    <div className="fixed navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -31,23 +50,55 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            {
-                navItems
-            }
+            {navItems}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">TaskQuest.com</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-         {
-            navItems
-         }
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className="navbar-end">
+          {user && (
+            <div className="flex items-center justify-center gap-2">
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 ring rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src={user.photoURL}
+                    />
+                  </div>
+                </div>
+                <ul className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content  rounded-box text-slate-600 bg-base-200">
+                  <li>
+                    <Link to="/dashboard/userProfile">Dashboard</Link>
+                  </li>
+                  <li>
+                    <button onClick={handleLogOut} className="">
+                      LogOut
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div className="font-semibold">
+                <p>
+                  {user.displayName}
+                </p>
+                <p>
+                  {user.email}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
+      <DarkModeToggle></DarkModeToggle>
     </div>
   );
 };
